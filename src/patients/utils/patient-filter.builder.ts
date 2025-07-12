2ea8b86b8498e3f10ea03ed09247f91c65d1a104
Marcos <marcostulli@hotmail.com>
2025-07-12 21:46:32 +0200

    Create service utils

---
+++ src/patients/utils/patient-filter.builder.ts
@@ -0,0 +1,37 @@
+import { FilterQuery } from 'mongoose';
+import { Patient } from '../schemas/patient.schema';
+import { PatientListDto } from '../dto/patient-list.dto';
+
+export class PatientFilterBuilder {
+  static buildFilter(filter: PatientListDto['filter']): FilterQuery<Patient> {
+    const query: FilterQuery<Patient> = {};
+
+    if (!filter) {
+      return query;
+    }
+
+    if (filter.firstName) {
+      query.firstName = { $regex: filter.firstName, $options: 'i' };
+    }
+    if (filter.lastName) {
+      query.lastName = { $regex: filter.lastName, $options: 'i' };
+    }
+    if (filter.email) {
+      query.email = { $regex: filter.email, $options: 'i' };
+    }
+    if (filter.phoneNumber) {
+      query.phoneNumber = { $regex: filter.phoneNumber, $options: 'i' };
+    }
+    if (filter.dobFrom || filter.dobTo) {
+      const dobFilter: Record<string, Date> = {};
+      if (filter.dobFrom) dobFilter.$gte = new Date(filter.dobFrom);
+      if (filter.dobTo) dobFilter.$lte = new Date(filter.dobTo);
+
+      if (Object.keys(dobFilter).length) {
+        query.dob = dobFilter;
+      }
+    }
+
+    return query;
+  }
+}
---
+++ src/patients/utils/patient-query.builder.ts
@@ -0,0 +1,39 @@
+import { FilterQuery, SortOrder } from 'mongoose';
+import { PatientFilterBuilder } from './patient-filter.builder';
+import { PatientListDto, SortDirection } from '../dto/patient-list.dto';
+import { Patient } from '../schemas/patient.schema';
+
+export class PatientQueryBuilder {
+  static buildSort(sort: PatientListDto['sort']): Record<string, SortOrder> {
+    if (sort?.field) {
+      return {
+        [sort.field]: sort.direction === SortDirection.DESC ? -1 : 1,
+      };
+    }
+    return { _id: 1 };
+  }
+
+  static buildPagination(pagination: PatientListDto['pagination']): {
+    skip: number;
+    limit: number;
+  } {
+    const page = pagination?.page ?? 1;
+    const take = pagination?.take ?? 10;
+
+    const skip = (page - 1) * take;
+    return { skip, limit: take };
+  }
+
+  static buildQuery(patientListDto: PatientListDto): {
+    filter: FilterQuery<Patient>;
+    sort: Record<string, SortOrder>;
+    skip: number;
+    limit: number;
+  } {
+    const filter = PatientFilterBuilder.buildFilter(patientListDto.filter);
+    const sort = this.buildSort(patientListDto.sort);
+    const { skip, limit } = this.buildPagination(patientListDto.pagination);
+
+    return { filter, sort, skip, limit };
+  }
+}
