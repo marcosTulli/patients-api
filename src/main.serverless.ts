1e7993cedaf0e37e274941cea1ae23cc825bc76c
Marcos <marcostulli@hotmail.com>
2025-11-26 17:53:54 +0100

    Troubleshooting timeout

--- src/main.serverless.ts
+++ src/main.serverless.ts
@@ -1,46 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
 import express from 'express';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
-import * as swaggerUi from 'swagger-ui-express';
 
 export async function createNestApplication() {
+  console.log('[Serverless] Creating express server...');
   const server = express();
+
+  console.log('[Serverless] Creating NestJS app...');
+  const start = Date.now();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Patients API')
-    .setDescription('Serve and operate patient data')
-    .setVersion('1.0')
-    .addApiKey(
-      { type: 'apiKey', name: 'x-api-key', in: 'header' },
-      'ApiKeyAuth',
-    )
-    .build();
-
-  const document: OpenAPIObject = SwaggerModule.createDocument(
-    app,
-    swaggerConfig,
-  );
-
-  server.use('/api', swaggerUi.serve);
-  server.get('/api', swaggerUi.setup(document));
-
-  server.get('/api-json', (_req, res) => {
-    res.json(document);
-  });
+  console.log(`[Serverless] NestJS app created in ${Date.now() - start}ms`);
 
   app.enableCors();
+
+  console.log('[Serverless] Initializing app...');
+  const initStart = Date.now();
   await app.init();
+  console.log(`[Serverless] App initialized in ${Date.now() - initStart}ms`);
+
   return server;
 }
